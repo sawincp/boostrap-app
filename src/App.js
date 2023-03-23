2c89de350c40acbc2b4bc276075a1c00d2a31890
Corey Sawin <sawincp@gmail.com>
2023-03-23 15:07:33 -0400

    Add CharacterContainer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import CharacterContainer from "./CharacterContainer";
 import Header from "./Header";
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <CharacterContainer />
 
     </div>
   );
---
+++ src/CharacterContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const CharacterContainer =() =>{
+
+    
+    return(
+        <Container></Container>
+        
+
+    )
+}
+
+export default CharacterContainer
